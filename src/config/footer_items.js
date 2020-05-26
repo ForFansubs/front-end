12417e7c0554c6d811e39f0eeba206adf53d7836
ayberktandogan <ayberk.tandogan@outlook.com>
2020-05-27 02:25:53 +0300

    Footer için ayarlanabilir linkler.

---
+++ src/config/footer_items.js
@@ -0,0 +1,10 @@
+// Bu dosyayı footer'ınıza herhangi bir link eklemek için kullanabilirsiniz.
+// Array içerisine koyacağınız objenin 
+// {
+//  title: ""   
+//  link: "" 
+// } biçiminde olması gerekiyor.
+
+module.exports = [
+
+]
