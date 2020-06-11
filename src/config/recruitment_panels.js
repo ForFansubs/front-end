1b27bffba228cffe33b5bc48098332eafbdb7423
ayberktandogan <ayberk.tandogan@outlook.com>
2020-06-11 18:26:28 +0300

    Ekip alımları modülerlik

---
+++ src/config/recruitment_panels.js
@@ -0,0 +1,13 @@
+// Bu dosya, ekip alımları sayfasını doldurmak için oluşturuldu.
+// {
+//     title: "",           -- Resmin altında, açıklamanın üstünde gözükecek yazı.
+//     subtitle: "",        -- Resmin ve title'ın altında gözükecek, açıklama yazısı.
+//     image: "",           -- Resim linki.
+//     link: "",            -- Kullanıcının butona tıkladığında yönlendirileceği link.
+//     button_text: "",     -- is_active 1'se buton gösterilir. Bu butonun içerisindeki yazı.
+//     is_active: 1         -- 0 yaparak butonu deaktif hale getirebilirsiniz.
+// }
+
+module.exports = [
+
+]
